94364f36f38bc524641b5718439058b3ea2b0f43
Juni <hjh4638@naver.com>
2015-06-17 11:42:47 +0900

    version start

---
+++ CatchWave/src/com/catchwave/view/LogoActivity.java
@@ -0,0 +1,15 @@
+package com.catchwave.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
